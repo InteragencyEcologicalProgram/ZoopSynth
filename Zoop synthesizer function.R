da352442d88234aa62922019baca3de568af8e55
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2019-07-03 17:08:41 -0700

    Using Madison's updated crosswalk with introduction, start, and end years, wrote code to change bad 0s to NAs depending on the year of sampling. Still need to check the logic within the case_when statements and make sure this is working correctly, and include FRP in these changes.

--- Zoop synthesizer function.R
+++ Zoop synthesizer function.R
@@ -35,14 +35,19 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
 
 # Setup -------------------------------------------------------------------
 
-  require(tidyverse)
+  ##NOTE THIS FUNCTION IS SUPER SLOW UNLESS YOU INSTALL THE DEVELOPER VERSION OF DPLYR DUE TO AN ISSUE WITH THE SUMMARISE FUNCTION
+  #`devtools::install_github("tidyverse/dplyr")`
+  require(tidyverse) 
   require(readxl)
   require(lubridate)
   
   # Load crosswalk key to convert each dataset's taxonomic codes to a
   # unified set of "Taxname" and "Lifestage" values.
   
-  crosswalk <- read_excel("new_crosswalk.xlsx", sheet = "Hierarchy2")
+  crosswalk <- read_excel("new_crosswalk.xlsx", sheet = "Hierarchy2")%>%
+    mutate_at(vars(c("EMPstart", "EMPend", "Intro", "FMWTstart", "FMWTend", "twentymmstart", "twentymmend", "twentymmstart2")), ~parse_date(as.character(.), format="%Y"))%>% ##NOTE MAY NEED TO CHANGE END DATES TO LAST DATE OF YEAR DEPENDING ON THEIR MEANING (LAST YEAR IN USE? OR FIRST YEAR NOT IN USE?)
+    mutate_at(vars(c("EMPstart", "FMWTstart", "twentymmstart", "twentymmstart2", "EMPend", "FMWTend", "twentymmend")), ~replace_na(., as_date(Inf)))%>% #Change any NAs for starts or ends to Infinity (i.e. never started or ended)
+    mutate_at(vars(c("Intro")), ~replace_na(., as_date(-Inf))) #Change any NAs in Intro date to -Inf (i.e., always been around)
   
   # Load station key to later incorporate latitudes and longitudes
   
@@ -113,17 +118,24 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
              Station, Region, Chl=`Chl-a`, CondBott = ECBottomPreTow, CondSurf = ECSurfacePreTow, Secchi, 
              Temperature, Volume = CBVolume, EMP, CPUE)%>% #Select for columns in common and rename columns to match
       left_join(crosswalk%>% #Add in Taxnames, Lifestage, and taxonomic info
-                  select(-FMWT, -twentymm, -FRP, -Level)%>% #only retain EMP codes
+                  select(-FMWT, -twentymm, -FRP, -Level, -FMWTstart, -FMWTend, -twentymmstart, -twentymmend, -twentymmstart2)%>% #only retain EMP codes
                   filter(!is.na(EMP))%>% #Only retain Taxnames corresponding to EMP codes
                   distinct(),
                 by="EMP")%>%
       mutate(Taxlifestage=paste(Taxname, Lifestage), #create variable for combo taxonomy x life stage
              SampleID=paste(Source, Station, Date))%>% #Create identifier for each sample
-      select(-EMP)%>% #Remove EMP taxa codes
       group_by_at(vars(-CPUE))%>%
       summarise(CPUE=sum(CPUE, na.rm=T))%>% #Some taxa now have the same names (e.g., CYCJUV and OTHCYCJUV)
                                             #so we now add those categories together.
-      ungroup()
+      ungroup()%>%
+      mutate(CPUE=case_when(
+        CPUE!=0 ~ CPUE, 
+        CPUE==0 & Date < Intro ~ 0,
+        CPUE==0 & Date >= Intro & Date < EMPstart ~ NA_real_,
+        CPUE==0 & Date >= EMPstart & Date < EMPend ~ 0,
+        CPUE==0 & Date >= EMPend ~ NA_real_
+      ))%>% 
+      select(-EMP, -EMPstart, -EMPend, -Intro) #Remove EMP taxa codes
   }
   
 
@@ -157,15 +169,22 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
              -TotalMeter, -Volume)%>% #transform from wide to long
       select(Source=Project, Year, Survey, Date, Datetime, Station, Region, TideCode, DepthBottom, CondSurf, CondBott, Temperature = TempSurf, Secchi, Turbidity, Microcystis, Volume, FMWT, CPUE)%>% #Select for columns in common and rename columns to match
       left_join(crosswalk%>% #Add in Taxnames, Lifestage, and taxonomic info
-                  select(-EMP, -twentymm, -FRP, -Level)%>% #only retain FMWT codes
+                  select(-EMP, -twentymm, -FRP, -Level, -EMPstart, -EMPend, -twentymmstart, -twentymmend, -twentymmstart2)%>% #only retain FMWT codes
                   filter(!is.na(FMWT))%>% #Only retain Taxnames corresponding to FMWT codes
                   distinct(),
                 by = "FMWT")%>%
       mutate(Taxlifestage=paste(Taxname, Lifestage), #create variable for combo taxonomy x life stage
              Microcystis=if_else(Microcystis=="6", "2", Microcystis), #Microsystis value of 6 only used from 2012-2015 and is equivalent to a 2 in other years, so just converting all 6s to 2s.
              SampleID=paste(Source, Station, Datetime))%>% #Create identifier for each sample
-      select(-FMWT)%>% #Remove FMWT taxa codes
-      ungroup()
+      ungroup()%>%
+      mutate(CPUE=case_when(
+        CPUE!=0 ~ CPUE, 
+        CPUE==0 & Date < Intro ~ 0,
+        CPUE==0 & Date >= Intro & Date < FMWTstart ~ NA_real_,
+        CPUE==0 & Date >= FMWTstart & Date < FMWTend ~ 0,
+        CPUE==0 & Date >= FMWTend ~ NA_real_
+      ))%>%
+      select(-FMWT, -FMWTstart, -FMWTend, -Intro) #Remove FMWT taxa codes
   }
   
 
@@ -220,18 +239,27 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
       left_join(enviro20mm, by = c("Station", "Date"))%>%
       rename(twentymm=CommonName)%>%
       left_join(crosswalk%>% #Add in Taxnames, Lifestage, and taxonomic info
-                  select(-EMP, -FMWT, -FRP, -Level)%>% #only retain 20mm codes
+                  select(-EMP, -FMWT, -FRP, -Level, -FMWTstart, -FMWTend, -EMPstart, -EMPend)%>% #only retain 20mm codes
                   filter(!is.na(twentymm))%>% #Only retain Taxnames corresponding to 20mm codes
                   distinct(),
                 by="twentymm")%>%
-      select(Date, Station, Volume, Temperature = Temp, CondSurf = TopEC, CondBott = BottomEC, Secchi, Turbidity, CPUE, Taxname, Lifestage, Phylum, Class, Order, Family, Genus, Species, SampleID)%>% #Select for columns in common and rename columns to match
+      select(Date, Station, Volume, Temperature = Temp, CondSurf = TopEC, CondBott = BottomEC, Secchi, Turbidity, CPUE, Taxname, Lifestage, Phylum, Class, Order, Family, Genus, Species, SampleID, twentymmend, twentymmstart, twentymmstart2, Intro)%>% #Select for columns in common and rename columns to match
       mutate(Source="20mm")%>% #add variable for data source
       mutate(Station=as.character(Station))%>%
       mutate(Taxlifestage=paste(Taxname, Lifestage))%>% #create variable for combo taxonomy x life stage
       group_by_at(vars(-CPUE))%>% #Some taxa names are repeated as in EMP so 
       summarise(CPUE=sum(CPUE, na.rm=T))%>% #this just adds up those duplications
       ungroup()%>%
-      mutate(SampleID=paste(Source, SampleID)) #Create identifier for each sample
+      mutate(SampleID=paste(Source, SampleID))%>% #Create identifier for each sample
+      mutate(CPUE=case_when(
+        CPUE!=0 ~ CPUE, 
+        CPUE==0 & Date < Intro ~ 0,
+        CPUE==0 & Date >= Intro & Date < twentymmstart ~ NA_real_,
+        CPUE==0 & Date >= twentymmstart & Date < twentymmend ~ 0,
+        CPUE==0 & Date >= twentymmend & Date < twentymmstart2 ~ NA_real_,
+        CPUE==0 & Date >= twentymmstart2 ~ 0 #20mm dataset had one case of a taxa starting, ending, and starting again
+      ))%>%
+      select(-twentymmend, -twentymmstart, -twentymmstart2)
   }
   
 
@@ -256,7 +284,7 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
              Station, CondSurf = SC, Secchi, pH, DO, Turbidity, Tide, Microcystis,
              Temperature = Temp, Volume = volume, FRP = CommonName, CPUE, SampleID)%>% #Select for columns in common and rename columns to match
       left_join(crosswalk%>% #Add in Taxnames, Lifestage, and taxonomic info
-                  select(-EMP, -twentymm, -FMWT, -Level)%>% #only retain FRP codes
+                  select(-EMP, -twentymm, -FMWT, -Level, -FMWTstart, -FMWTend, -twentymmstart, -twentymmend, -twentymmstart2, -EMPstart, -EMPend)%>% #only retain FRP codes
                   filter(!is.na(FRP))%>% #Only retain Taxnames corresponding to 20mm codes
                   distinct(),
                 by = "FRP")%>%
@@ -266,6 +294,7 @@ Zooper<-function(Sources=c("EMP", "FRP", "FMWT", "20mm"), Daterange=c(NA, NA), M
       summarise(CPUE=sum(CPUE, na.rm=T))%>% #this just adds up those duplications
       ungroup()%>%
       mutate(SampleID=paste(Source, SampleID)) #Create identifier for each sample
+      
   }
   
 
