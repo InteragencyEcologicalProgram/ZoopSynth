5e8513c9a4f1d1612e6baa6ea7b81abe12360ce7
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2019-07-19 08:26:46 -0700

    Split data production into separate reactive object from graph

--- shinyzoops/app.R
+++ shinyzoops/app.R
@@ -27,18 +27,18 @@ source("../Zoop synthesizer function.R")
 
 # Define UI for application that draws a scaterplot and allows you to download data
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Zooplankton"),
-
+    
     # check boxes where you choose data you want
     sidebarLayout(
         sidebarPanel(
             radioButtons("Datatype", "Data Type", choices = c("Taxa", "Community"), selected = "Community",
                          inline = TRUE),
             checkboxGroupInput("Sources",
-                        "Sources:",
-                        choices = c("EMP", "FRP", "FMWT", "TNS", "20mm")),
+                               "Sources:",
+                               choices = c("EMP", "FRP", "FMWT", "TNS", "20mm")),
             checkboxGroupInput("Filters",
                                "Filters:",
                                choices = c("Months", "Surface_salinity", "Latitude", "Longitude", "Dates")),
@@ -60,12 +60,12 @@ ui <- fluidPage(
                              dateRangeInput("Daterange", label = "Date range", 
                                             start = "1972-01-01", end = "2018-12-31", startview = "year")),
             actionButton("Run", "Run"),
-        downloadButton("download", "Download")
-            ),
-
+            downloadButton("download", "Download")
+        ),
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            plotOutput("distPlot")
         ),
         position = "left",
         fluid = F
@@ -80,7 +80,7 @@ server <- function(input, output) {
     })
     
     #Using eventReactive so app only updates when "Run" button is clicked, letting you check all the boxes you want before running the app
-    zooplot <- eventReactive(input$Run, {
+    plotdata <- eventReactive(input$Run, {
         
         #select the data the user wants
         
@@ -92,17 +92,16 @@ server <- function(input, output) {
                        Latrange = ifelse(rep("Latitude"%in%input$Filters, 2), input$Latrange, c(NA, NA)), 
                        Longrange = ifelse(rep("Longitude"%in%input$Filters, 2), input$Longrange, c(NA, NA)), 
                        Shiny=T)
-        
-        # draw the scatterplot of the the critters
-        ggplot(data, aes(x=Date, y = CPUE)) +
+    })
+    
+    output$distPlot <- renderPlot({
+        plotdata()%>%
+            ggplot(aes(x=Date, y = CPUE)) +
             geom_point(aes(shape = Source))+
             theme_bw()+
             theme(panel.grid=element_blank())
     })
-        
-        output$distPlot <- renderPlot({
-            zooplot()
-        })
+    
     
     output$download = downloadHandler(
         filename = function() {
@@ -110,13 +109,13 @@ server <- function(input, output) {
         },
         content = function(file) {
             data    <- Zooper(Data = input$Datatype, 
-                           Sources = input$Sources, 
-                           Daterange= ifelse("Dates"%in%input$Filters, input$Daterange, c(NA, NA)),
-                           Months = ifelse("Months"%in%input$Filters, as.integer(input$Months), NA),  
-                           SalSurfrange = ifelse("Surface salinity"%in%input$Filters, input$SalSurfrange, NA),
-                           Latrange = ifelse("Latitude"%in%input$Filters, input$Latrange, NA), 
-                           Longrange =  ifelse("Longitudee"%in%input$Filters, input$Longrange, NA), 
-                           Shiny=T)
+                              Sources = input$Sources, 
+                              Daterange= ifelse("Dates"%in%input$Filters, input$Daterange, c(NA, NA)),
+                              Months = ifelse("Months"%in%input$Filters, as.integer(input$Months), NA),  
+                              SalSurfrange = ifelse("Surface salinity"%in%input$Filters, input$SalSurfrange, NA),
+                              Latrange = ifelse("Latitude"%in%input$Filters, input$Latrange, NA), 
+                              Longrange =  ifelse("Longitudee"%in%input$Filters, input$Longrange, NA), 
+                              Shiny=T)
             write.csv(data, file)
         }
     )
