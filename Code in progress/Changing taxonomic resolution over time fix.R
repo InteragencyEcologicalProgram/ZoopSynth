440bddaa767830a8e2e309f22d1f618e1dff76cb
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-01-28 11:27:25 -0800

    Made progress on solutions to changing taxonomic resolution over time

--- Code in progress/Changing taxonomic resolution over time fix.R
+++ Code in progress/Changing taxonomic resolution over time fix.R
@@ -1,11 +1,12 @@
 require(tidyverse) 
 require(readxl)
 require(lubridate)
+require(zooper)
 
 # Load crosswalk key to convert each dataset's taxonomic codes to a
 # unified set of "Taxname" and "Lifestage" values.
 
-crosswalk <- read_excel("Data/new_crosswalk.xlsx", sheet = "Hierarchy2")%>%
+crosswalk <- zooper::crosswalk%>%
   mutate_at(vars(c("EMPstart", "EMPend", "Intro", "FMWTstart", "FMWTend", "twentymmstart", "twentymmend", "twentymmstart2")), ~parse_date(as.character(.), format="%Y"))%>%
   mutate_at(vars(c("EMPstart", "FMWTstart", "twentymmstart", "twentymmstart2", "EMPend", "FMWTend", "twentymmend")), ~replace_na(., as_date(Inf)))%>% #Change any NAs for starts or ends to Infinity (i.e. never started or ended)
   mutate(EMPend = if_else(is.finite(EMPend), EMPend+years(1), EMPend))%>% #Change end dates to beginning of next year (first day it was not counted)
@@ -13,14 +14,48 @@ crosswalk <- read_excel("Data/new_crosswalk.xlsx", sheet = "Hierarchy2")%>%
   mutate(twentymmend = if_else(is.finite(twentymmend), twentymmend+years(1), twentymmend))%>% #Change end dates to beginning of next year (first day it was not counted)
   mutate(Intro=replace_na(Intro, as_date(-Inf))) #Change any NAs in Intro date to -Inf (i.e., always been around)
 
-#Calculate years when taxa were not counted
-EMP<-crosswalk%>%
-  filter(!is.na(EMP))%>%
-  select(Taxname, Lifestage, EMPstart, EMPend, Intro)%>%
-  mutate(Taxlifestage=paste(Taxname, Lifestage),
-         Years=sapply(1:nrow(.), function(x) ifelse(is.finite(EMPstart[x]), ifelse(is.finite(Intro[x]), paste(year(Intro[x]):year(EMPstart[x]), collapse=","), paste(1950:year(EMPstart[x]), collapse=",")), NA_character_)))%>%
-  mutate(Yearsend=sapply(1:nrow(.), function(x) ifelse(is.finite(EMPend[x]), paste(year(EMPend[x]):year(Sys.Date()), collapse=","), NA_character_)))%>%
-  mutate(Years=sapply(1:nrow(.), function (x) paste(unique(c(str_split(Years[x], ",")[[1]], str_split(Yearsend[x], ",")[[1]])), collapse=",")))%>%
-  select(Taxlifestage, Years)%>%
-  group_by(Taxlifestage)%>%
-  summarise(Years=ifelse(n()>1, paste(Reduce(intersect, (sapply(1:n(), function(x) str_split(Years[x], ",")[[1]]))), collapse=","), Years))
+#Calculate years when taxa were not counted but should have been
+
+Uncounter<- function(Source, SizeClass){
+  start<-sym(paste0(Source, "start"))
+  start<-enquo(start)
+  end<-sym(paste0(Source, "end"))
+  end<-enquo(end)
+  dataset<-sym(paste(Source, SizeClass, sep="_"))
+  dataset<-enquo(dataset)
+  
+  out<-crosswalk%>%
+    filter(!is.na(!!dataset))%>%
+    mutate(!!start := if_else(!is.finite(!!start), Sys.Date(), !!start))%>%
+    mutate(Intro = if_else(!is.finite(Intro) | Intro > !!start, !!start, Intro),
+           !!end := if_else(!is.finite(!!end), Sys.Date(), !!end))%>%
+    select(Taxname, Lifestage, !!start, !!end, Intro, !!dataset)%>%
+    mutate(Taxlifestage=paste(Taxname, Lifestage))%>%
+    group_by(Taxname, Lifestage, !!dataset, Taxlifestage, !!start, !!end, Intro, n = row_number())%>%
+    do(tibble(Years = list(c(seq(year(.$Intro), year(.[[paste0(Source, "start")]]), by = 1), seq(year(.[[paste0(Source, "end")]]), year(Sys.Date()), by=1)))))%>%
+    ungroup()%>%
+    select(-n)%>%
+    unnest(cols=c(Years))%>%
+    mutate(Years=ifelse(Years==year(Intro) | Years==year(Sys.Date()), NA, Years))%>%
+    group_by(Taxlifestage, Taxname, Lifestage, !!dataset)%>%
+    summarise(Years=list(unique(Years)))%>%
+    group_by(Taxlifestage, Taxname, Lifestage)%>%
+    summarise(Years=list(Reduce(intersect, Years)))%>%
+    ungroup()%>%
+    unnest(cols=c(Years))%>%
+    filter(!is.na(Years))%>%
+    mutate(Source=Source,
+           SizeClass=SizeClass)%>%
+    group_by(Taxlifestage, Taxname, Lifestage, Source, SizeClass)%>%
+    summarise(Years=list(unique(Years)))
+  return(out)
+}
+
+datasets<-zoopComb%>%
+  mutate(names=paste(Source, SizeClass, sep="_"))%>%
+  select(names, Source, SizeClass)%>%
+  filter(Source%in%c("EMP", "FMWT", "twentymm"))%>%
+  distinct()
+
+test<-map2_dfr(datasets$Source, datasets$SizeClass, Uncounter)
+
