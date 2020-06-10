87c15f6ebeb5264f9986d7b19114073530778fa4
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-06-10 14:40:47 -0700

    Updating CB vs Pump plot for data paper

--- Code in progress/Plotting CB vs Pump sample efficiency.R
+++ Code in progress/Plotting CB vs Pump sample efficiency.R
@@ -1,109 +1,51 @@
 require(tidyverse)
-require(readxl)
-require(lubridate)
-require(dtplyr)
+require(zooper)
 require(broom)
 require(RColorBrewer)
 
-EMP_CB <- read_excel("Data/1972-2018CBMatrix.xlsx", 
-                     sheet = "CB CPUE Matrix 1972-2018", 
-                     col_types = c("numeric","numeric", "numeric", "numeric", "date", 
-                                   "text", "text", "text", "numeric", "text", "text",
-                                   "text", rep("numeric", 62)))%>%
-  rename(Volume=CBVolume)
+EMP<-zooper::zoopComb%>%
+  left_join(zooper::zoopEnvComb, by=c("Source", "SampleID"))%>%
+  filter(Source=="EMP" & SizeClass%in%c("Micro", "Meso"))
 
-EMP_Pump <- read_excel("Old data and code/1972-2018Pump Matrix.xlsx", 
-                       sheet = "Pump CPUE Matrix 1972-2018", 
-                       col_types = c("numeric","numeric", "numeric", "numeric", "date", 
-                                     "text", "text", "text", "numeric", 
-                                     "text", rep("numeric", 36)))%>%
-  rename(Date=SampleDate, Volume=PumpVolume)
+Taxa<-EMP%>%
+  group_by(SizeClass)%>%
+  summarise(Taxa=list(unique(Taxlifestage)), .groups="drop")
 
-crosswalk <- zooper::crosswalk%>%
-  select(EMP=EMP_Meso, Taxname, Lifestage, EMPstart, EMPend, Intro)%>%
-  filter(!is.na(EMP))%>%
-  mutate(EMPstart=ifelse(EMP=="OTHCYCAD", 1975, EMPstart))%>% #Pump started counting OTHCYCAD in 1975 while CB started in 1972 so using the later year for both
-  mutate_at(vars(c("EMPstart", "EMPend", "Intro")), ~parse_date(as.character(.), format="%Y"))%>%
-  mutate_at(vars(c("EMPstart", "EMPend")), ~replace_na(., as_date(Inf)))%>% #Change any NAs for starts or ends to Infinity (i.e. never started or ended)
-  mutate(EMPend = if_else(is.finite(EMPend), EMPend+years(1), EMPend))%>% #Change end dates to beginning of next year (first day it was not counted)
-  mutate(Intro=replace_na(Intro, as_date(-Inf))) #Change any NAs in Intro date to -Inf (i.e., always been around)
+Taxa<-intersect(Taxa$Taxa[1][[1]], Taxa$Taxa[2][[1]])
 
-#Select columns in common between two datasets, rename taxa columns with distinct names
+EMP<-EMP%>%
+  filter(Taxlifestage%in%Taxa)
 
-#Create vector of taxonomic variables
-Taxvars<-c("LIMNOSPP", "LIMNOSINE", "LIMNOTET", "OITHDAV", "OITHSIM", "OITHSPP", "OTHCYCAD", "ALLCYCADULTS", "HARPACT", "CYCJUV", "LIMNOJUV", "OITHJUV", "OTHCYCJUV", "ALLCYCJUV", "COPNAUP", "EURYNAUP", "OTHCOPNAUP", "PDIAPNAUP", "SINONAUP", "ALLCOPNAUP", "ASPLANCH", "KERATELA", "OTHROT", "POLYARTH", "SYNCH", "SYNCHBIC", "TRICHO", "ALLROTIFERS", "BARNNAUP")
+Samples<-EMP%>%
+  group_by(SizeClass)%>%
+  summarise(Samples=list(unique(SampleID)), .groups="drop")
 
-EMP_CB2<-EMP_CB%>%
-  select(intersect(names(.), names(EMP_Pump)))%>% #select columns in common between two datasets
-  select(-Survey, -SurveyCode, -SurveyRep, -EZStation, -Core)%>% #Remove some variables
-  mutate(SampleID=paste(Date, Station))%>% #Create stationID identifier
-  rename_at(vars(Taxvars),
-            ~paste0(., "_CB"))%>% #append "_CB" to taxonomic variables
-  select(-Volume)
+Samples<-intersect(Samples$Samples[1][[1]], Samples$Samples[2][[1]])
 
-#Do the same for the Pump samples, except total pump catch shouldn't matter 
-EMP_Pump2<-EMP_Pump%>%
-  select(intersect(names(.), names(EMP_CB)))%>%
-  select(-Survey, -SurveyCode, -SurveyRep, -EZStation, -Core)%>%
-  mutate(SampleID=paste(Date, Station))%>%
-  rename_at(vars(Taxvars),
-            ~paste0(., "_Pump"))%>%
-  select(-Volume)
-
-
-#Ensure that the same samples are present from both methods
-EMP_CB3<-EMP_CB2%>%
-  filter(SampleID%in%unique(EMP_Pump2$SampleID))
-
-EMP_Pump3<-EMP_Pump2%>%
-  filter(SampleID%in%unique(EMP_CB2$SampleID))
-
-#Join pump and CB data
-EMP<-EMP_CB3%>%
-  inner_join(EMP_Pump3, 
-             by = c("Year", "Date", "Station", "Region", "Secchi", "Chl-a", "Temperature", "ECSurfacePreTow", "ECBottomPreTow", "SampleID"))%>%
-  select(-Region, -Secchi, -`Chl-a`, -Temperature, -ECSurfacePreTow, -ECBottomPreTow, -Station)%>%
-  pivot_longer(cols= c(ends_with("_CB"), ends_with("_Pump")), names_to=c("EMP", "Method"), names_sep = "_", values_to="CPUE")%>%
-  left_join(crosswalk%>% #Add in Taxnames, Lifestage, and taxonomic info
-              select(EMP, Lifestage, Taxname, Intro, EMPstart, EMPend)%>% #only retain EMP codes
-              filter(!is.na(EMP))%>% #Only retain Taxnames corresponding to EMP codes
-              distinct(),
-            by="EMP")%>%
-  filter(!is.na(Taxname))%>% #Should remove all the summed categories in original dataset
+EMP<-EMP%>%
+  filter(SampleID%in%Samples)%>%
   mutate(Taxname_label=ifelse(Taxname%in%c("Asplanchna", "Keratella", "Limnoithona", "Limnoithona sinensis", "Limnoithona tetraspina", "Oithona", "Oithona davisae", "Oithona similis", "Polyarthra", "Synchaeta", "Synchaeta bicornis", "Trichocerca", "Eurytemora affinis", "Pseudodiaptomus", "Sinocalanus doerrii"), paste0("italic('", Taxname, "')"),
-                             Taxname))%>% #create variable for combo taxonomy x life stage and make some names italicized for graph. 
-  mutate(CPUE=case_when(
-    CPUE!=0 ~ CPUE, 
-    CPUE==0 & Date < Intro ~ 0,
-    CPUE==0 & Date >= Intro & Date < EMPstart ~ NA_real_,
-    CPUE==0 & Date >= EMPstart & Date < EMPend ~ 0,
-    CPUE==0 & Date >= EMPend ~ NA_real_
-  ))%>% 
-  select(-EMP, -EMPstart, -EMPend, -Intro)%>% #Remove EMP taxa codes 
-  lazy_dt()%>% #Speed up code using dtplyr package that takes advantage of data.table speed
-  group_by_at(vars(-CPUE))%>%
-  summarise(CPUE=sum(CPUE, na.rm=TRUE))%>% #Some taxa now have the same names (e.g., CYCJUV and OTHCYCJUV)
-  #so we now add those categories together.
-  ungroup()%>%
-  as_tibble() #required to finish operation after lazy_dt()
+                              Taxname)) #create variable for combo taxonomy x life stage and make some names italicized for graph. 
 
 EMP_sum<-EMP%>%
-  group_by(Method, Taxname, Lifestage, Taxname_label)%>%
-  summarise(CPUE=sum(CPUE, na.rm=T))%>%
-  ungroup()%>%
-  mutate(Method = recode(Method, CB = "Meso", Pump = "Micro"))%>%
+  group_by(SizeClass, Taxname, Lifestage, Taxname_label)%>%
+  summarise(N_detect=length(which(CPUE>0)),
+            CPUE=sum(CPUE, na.rm=T), .groups="drop")%>%
   group_by(Taxname, Lifestage, Taxname_label)%>%
-  mutate(Total=sum(CPUE, na.rm=T))%>%
+  mutate(Total=sum(CPUE, na.rm=T),
+         N_detect=sum(N_detect))%>%
   ungroup%>%
   mutate(Proportion=CPUE/Total)%>%
   arrange(Taxname, Lifestage)%>%
-  mutate(Taxname_label=factor(Taxname_label, levels=unique(Taxname_label)))
+  mutate(Taxname_label=factor(Taxname_label, levels=unique(Taxname_label)),
+         N_detect=format(N_detect, big.mark=","))
 
 p<-ggplot(EMP_sum)+
-  geom_bar(aes(x=Method, y=Proportion, fill=Method), stat="identity")+
+  geom_bar(aes(x=SizeClass, y=Proportion, fill=SizeClass), stat="identity")+
+  geom_text(aes(label=format(round(CPUE), big.mark=","), x=SizeClass, y=1.1), size=3)+
   facet_wrap(~Taxname_label*Lifestage, labeller = label_parsed, ncol=4)+
   scale_fill_manual(values=brewer.pal(3, "PRGn")[c(1,3)])+
-  scale_y_continuous(limits=c(0,1), expand=c(0,0))+
+  scale_y_continuous(limits=c(0,1.2), expand=c(0,0), breaks=c(0,0.25, 0.5, 0.75, 1))+
   ylab("Proportion of total CPUE")+
   xlab("Collection method")+
   theme_bw()+
@@ -112,7 +54,54 @@ p
 ggsave(p, file="Code in progress/Pump CB CPUEs bar.png", device = "png", units="in", height=8, width=7.5)
 
 
+# using zooper data -------------------------------------------------------
+
+EMP2<-zooper::zoopComb%>%
+  left_join(zooper::zoopEnvComb, by=c("Source", "SampleID"))%>%
+  filter(Source=="EMP" & SizeClass%in%c("Micro", "Meso"))
+
+Taxa<-EMP2%>%
+  group_by(SizeClass)%>%
+  summarise(Taxa=list(unique(Taxlifestage)), .groups="drop")
+
+Taxa<-intersect(Taxa$Taxa[1][[1]], Taxa$Taxa[2][[1]])
+
+EMP2<-EMP2%>%
+  filter(Taxlifestage%in%Taxa)
+
+Samples<-EMP2%>%
+  group_by(SizeClass)%>%
+  summarise(Samples=list(unique(SampleID)), .groups="drop")
+
+Samples<-intersect(Samples$Samples[1][[1]], Samples$Samples[2][[1]])
+
+EMP2<-EMP2%>%
+  filter(SampleID%in%Samples)
+
+EMP3<-EMP2%>%
+  mutate(Taxname_label=ifelse(Taxname%in%c("Asplanchna", "Keratella", "Limnoithona", "Limnoithona sinensis", "Limnoithona tetraspina", "Oithona", "Oithona davisae", "Oithona similis", "Polyarthra", "Synchaeta", "Synchaeta bicornis", "Trichocerca", "Eurytemora affinis", "Pseudodiaptomus", "Sinocalanus doerrii"), paste0("italic('", Taxname, "')"),
+                              Taxname)) #create variable for combo taxonomy x life stage and make some names italicized for graph. 
+  
+EMP_sum2<-EMP3%>%
+  group_by(SizeClass, Taxname, Lifestage, Taxname_label)%>%
+  summarise(N_detect=length(which(CPUE>0)),
+            CPUE=sum(CPUE, na.rm=T))%>%
+  ungroup()%>%
+  group_by(Taxname, Lifestage, Taxname_label)%>%
+  mutate(Total=sum(CPUE, na.rm=T),
+         N_detect=sum(N_detect))%>%
+  ungroup%>%
+  mutate(Proportion=CPUE/Total)%>%
+  arrange(Taxname, Lifestage)%>%
+  mutate(Taxname_label=factor(Taxname_label, levels=unique(Taxname_label)),
+         N_detect=format(N_detect, big.mark=","))
+
+# Statistics --------------------------------------------------------------
+
+
+
 EMP_xyplot<-EMP%>%
+  mutate(Taxlifestage=paste(Taxname, Lifestage))%>%
   spread(key = Method, value = CPUE)%>%
   select(-Taxname, -Lifestage, -Year)%>%
   mutate(CB_l=log(CB+1),
@@ -160,3 +149,9 @@ p<-ggplot(EMP_xyplot, aes(x=CB_l, y=Pump_l, group=Taxlifestage))+
 
 ggsave(p, file="Pump CB CPUEs.png", device = "png", units="in", height=8, width=10)
 
+
+# stats -------------------------------------------------------------------
+
+EMP_stats<-EMP%>%
+  select(-Taxname_label)%>%
+  pivot_wider(names_from=Method)
