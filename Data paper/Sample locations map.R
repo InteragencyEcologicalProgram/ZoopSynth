64241ecabc74d9a5d6b1fb93ee6a2f80ea81d7e2
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-08-17 09:54:47 -0700

    Trimming out old code

--- Data paper/Sample locations map.R
+++ Data paper/Sample locations map.R
@@ -1,83 +1,8 @@
-require(leaflet)
-require(mapview)
-require(RColorBrewer)
-require(readxl)
 require(tidyverse)
-require(ggplot2)
 require(ggspatial)
 require(sf)
 require(ggrepel)
-library(maps)
-
-Stations <- zooper::stations%>%
-  filter(Source!="YBFMP")%>%
-  #bind_rows(zooper::stationsEMPEZ%>%
-  #            mutate(Station=paste(Station, Date),
-  #                   Source="EMP")%>%
-  #            select(-Date))%>%
-  mutate(Source=recode(Source, twentymm="20mm"))%>%
-  drop_na()
-
-pal<-colorFactor(brewer.pal(5, "Dark2"), Stations$Source)
-
-addLegendCustom <- function(map, sizes, ...){
-  labelAdditions <- paste0("<div style='display: inline-block;height: ", sizes, "px;margin-top: 4px;line-height: ", sizes, "px;'>", labels, "</div>")
-  
-  return(addLegend(map, colors = colorAdditions, labels = labelAdditions, 
-                   opacity = opacity, position=position, title=title))
-}
-
-#Zoomed in
-p<-leaflet(data=Stations)%>%
-  addProviderTiles("Esri.WorldGrayCanvas")%>%
-  setView(lng=-121.873, lat=38.190039, zoom=11)%>%
-  addCircleMarkers(lat=~Latitude, lng=~Longitude, fillColor = ~pal(Source), color = "Black", radius=5, weight=2, fillOpacity = 0.4)%>%
-  addLegend("topleft", pal = pal, values = ~Source)
-
-mapview::mapshot(p, file="Samples map in.png", vheight=1333, vwidth=1690)
-
-tags$head(
-  tags$style(
-    ".sam{
-          line-height: 30px;
-          font-size: 30px;
-          width: 30px;
-          height: 30px;
-    }"
-  )
-)
-
-## To increase legend size, add the following to R/R-3.6.1/library/leaflet/htmlwidgets/lib/leafletfix/leafletfix.css
-#.legend-big {
-#  line-height: 30px;
-#  font-size: 30px;
-#  color: #555;
-#}
-
-#.legend-big i {
-#  width: 30px;
-#  height: 30px;
-#  margin-right: 4px;
-#  opacity: 0.7;
-#  display: inline-block;
-#  vertical-align: top;
-#  /*For IE 7*/
-#    zoom: 1;
-#  *display: inline;
-#}
-
-#Zoomed out
-p<-leaflet(data=Stations)%>%
-  addProviderTiles("Esri.WorldGrayCanvas")%>%
-  setView(lng=-121.873, lat=38.190039, zoom=10)%>%
-  addCircleMarkers(lat=~Latitude, lng=~Longitude, fillColor = ~pal(Source), color = "Black", radius=5, weight=2, fillOpacity = 0.4)%>%
-  addLegend("topleft", pal = pal, values = ~Source, className = "info legend, legend-big")
-p
-mapview::mapshot(p, file="Code in progress/Samples map out.png", vheight=800, vwidth=1000)
-
-
-
-# ggplot2 version ---------------------------------------------------------
+require(maps)
 
 Stations<-zooper::stations%>%
   filter(Source!="YBFMP")%>%
@@ -94,11 +19,7 @@ labels<-tibble(label=c("San Francisco Bay", "San Pablo Bay", "Suisun Bay", "Suis
                Latitude=c(37.9, 38.07, 38.08, 38.2, 38.046, 38.23, 38.5, 37.9, 38.23), 
                Longitude=c(-122.4, -122.4, -122.05, -122.05, -121.9, -121.675, -121.56, -121.325, -122.3),
                label_lat=c(37.9, 38.11, 38.15, 38.25, 38, 38.2, 38.5, 37.85, 38.25), 
-               label_lon=c(-122.25, -122.38, -122.18, -122.18, -122, -121.8, -121.73, -121.42, -122.37))#%>%
-  #bind_rows(Stations%>%
-  #            st_drop_geometry()%>%
-  #            select(Latitude, Longitude)%>%
-  #            mutate(label=""))
+               label_lon=c(-122.25, -122.38, -122.18, -122.18, -122, -121.8, -121.73, -121.42, -122.37))
 
 states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))%>%
   st_transform(crs=st_crs(base))
--- Data paper/Sampling effort figure.R
+++ Data paper/Sampling effort figure.R
@@ -5,48 +5,9 @@ require(ggplot2)
 require(patchwork)
 require(purrr)
 
-Effort<-zoopEnvComb%>%
-  mutate(Month=month(Date, label=T),
-         Source=recode(Source, twentymm="20mm"))%>%
-  group_by(Source, Month, Station)%>%
-  summarise(N=n(), N_years=length(unique(Year)), .groups="drop")%>%
-  mutate(N_yearly=N/N_years)%>%
-  group_by(Source, Month)%>%
-  summarise(N=mean(N_yearly), .groups="drop")
-
-p_effort<-ggplot(Effort, aes(x=Month, y=Source, fill=N))+
-  geom_tile()+
-  geom_text(aes(label=format(round(N,2)), color=if_else(N>4, TRUE, FALSE)), show.legend = FALSE)+
-  scale_x_discrete(expand=expansion(0,0))+
-  scale_y_discrete(expand=expansion(0,0), limits=c("FRP", "STN","FMWT", "20mm", "EMP"))+
-  ylab("Survey")+
-  scale_fill_viridis_c(name="Average number \nof samples per \nyear and station")+
-  scale_color_manual(values=c("White", "black"))+
-  theme_bw()+
-  theme(panel.grid=element_blank(), legend.position = "none")
-
-Years<-zoopEnvComb%>%
-  mutate(Source=recode(Source, twentymm="20mm"))%>%
-  group_by(Source)%>%
-  summarise(min_year=min(Year), max_year=max(Year), .groups="drop")
-
-p_years<-ggplot(Years, aes(y=Source, x=min_year, xend=max_year, yend=Source))+
-  geom_segment(size=1)+
-  geom_point(size=2)+
-  scale_y_discrete(limits=c("FRP", "STN","FMWT", "20mm", "EMP"))+
-  xlab("Year")+
-  ylab("")+
-  theme_bw()+
-  theme(panel.grid.major.y =element_blank(), axis.text.y=element_blank(), axis.ticks.y = element_blank())
-
-p<-p_effort+p_years+plot_layout(widths=c(0.8, 0.2))+plot_annotation(tag_levels = "A", tag_suffix = ")")
-
-ggsave(plot=p, filename="Data paper/Sampling effort.png", device="png", units="in", height=5, width=10)
-
-
 # Plotting monthly sampling effort by year and survey ---------------------
 
-Effort2<-zoopEnvComb%>%
+Effort<-zoopEnvComb%>%
   mutate(Month=month(Date, label=T),
          Source=recode(Source, twentymm="20mm"))%>%
   group_by(Source, Month, Year, Station)%>%
@@ -58,7 +19,7 @@ Effort2<-zoopEnvComb%>%
 
 axis_lims<-tibble(Source=c("EMP", "20mm","FMWT", "STN", "FRP"), N=c(2.7, 2.7, 2.7, 2.7, 7.2), Year=2000)%>%
   mutate(Source=factor(Source, levels=c("EMP", "20mm","FMWT", "STN", "FRP")))#%>%
-  #complete(Source, Year, Month=unique(Effort2$Month))%>%
+  #complete(Source, Year, Month=unique(Effort$Month))%>%
   #fill(N)
 
 effort_plot<-function(source){
@@ -68,7 +29,7 @@ effort_plot<-function(source){
                    source=="STN" ~ "#984ea3",
                    source=="FRP" ~ "#ff7f00")
   
-  p<-ggplot(filter(Effort2, Source==source), aes(x=Year, y=N))+
+  p<-ggplot(filter(Effort, Source==source), aes(x=Year, y=N))+
     geom_line(size=1, color=Color)+
     geom_point(size=1, color=Color)+
     geom_blank(data=filter(axis_lims, Source==source))+
@@ -99,4 +60,4 @@ p<-purrr::map(c("EMP", "20mm","FMWT", "STN", "FRP"), effort_plot)
 
 p2<-wrap_plots(p)+plot_layout(nrow=1)
 
-ggsave(plot=p2, filename="Data paper/Sampling effort2.png", device="png", units="in", height=10, width=9)
+ggsave(plot=p2, filename="Data paper/Sampling Effort.png", device="png", units="in", height=10, width=9)
