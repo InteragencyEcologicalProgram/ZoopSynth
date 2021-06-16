3b578dfa0d849f0b449e308869244bc2d9083901
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2021-06-16 15:14:54 -0700

    Updating the installable shiny app

--- Creating a windows program from shiny app.R
+++ Creating a windows program from shiny app.R
@@ -11,16 +11,14 @@ require(RInno)
 
 #First must create "Zoop App" folder and "www" subfolder.
 
-file.copy(from=file.path(getwd(), "app.R"), to=file.path("..", "Zoop App"), overwrite = TRUE)
-file.copy(from=file.path(getwd(), "www", c("Logo.jpg", "zooper.png")), to=file.path("..", "Zoop App", "www"), overwrite = TRUE)
+file.copy(from=file.path("app", "app.R"), to=file.path("..", "Zoop App"), overwrite = TRUE)
+file.copy(from=file.path("app", "www", c("Logo.jpg", "zooper.png")), to=file.path("..", "Zoop App", "www"), overwrite = TRUE)
 
 
 # Create application ------------------------------------------------------
 
 
-# Must use ggiraph v. 0.6.1
-# Before running `Create_app`, be sure to download windows binary (.zip) version of 
-# ggiraph 0.6.1 (from https://mran.microsoft.com/timemachine, go to 05/01/2019)
+# First must build a binary version of the zooper package
 # and place that binary zipped version within the folder ~/Zoop App/bin.
 
 #While running, say "no" to all prompts about updating packages
